df591f048111711a97a5f36feedda70e808f2fa4
Andi Surya <andi502.as@gmail.com>
2024-07-05 22:16:59 +0700

    (feat): scrap news using Gnews, write to json

---
+++ scrapper.py
@@ -0,0 +1,20 @@
+from gnews import GNews
+import json
+
+# Config GNews
+google_news = GNews()
+google_news.country = 'United States'
+google_news.language = 'English'
+google_news.start_date = (2024, 1, 1)
+google_news.max_results = 10
+
+# Fetch news using GNews
+us_news = google_news.get_news('United State Election')
+
+# Convert to json
+us_news_json = json.dumps(us_news, indent=4)
+
+# Write to json
+with open("us_news.json", "w") as outfile:
+    outfile.write(us_news_json)
+
